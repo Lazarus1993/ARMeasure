09a9a863c81392f0aa2066ee50ad115b096cbf32
Lazarus1993 <ashutoshburde@gmail.com>
2019-10-01 13:55:01 -0700

    AR app to measure distance

--- ARMeasure/ViewController.swift
+++ ARMeasure/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
+    var dots = [SCNNode]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -23,11 +25,65 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
         
-        // Set the scene to the view
-        sceneView.scene = scene
+//        // Create a new scene
+//        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+//
+//        // Set the scene to the view
+//        sceneView.scene = scene
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first{
+            let touchLocation = touch.location(in: sceneView)
+            
+            let hitTestResult = sceneView.hitTest(touchLocation, types: .featurePoint)
+            
+            if let hitResult = hitTestResult.first{
+                addDot(at: hitResult)
+            }
+        }
+    }
+    
+    func addDot(at hitResult: ARHitTestResult){
+        let sphere = SCNSphere(radius: 0.005)
+        
+        let material = SCNMaterial()
+        
+        material.diffuse.contents = UIColor.red
+        
+        sphere.materials = [material]
+        
+        let node = SCNNode()
+        
+        node.position = SCNVector3(x: hitResult.worldTransform.columns.3.x, y: hitResult.worldTransform.columns.3.y, z: hitResult.worldTransform.columns.3.z)
+        
+        node.geometry = sphere
+        
+        sceneView.scene.rootNode.addChildNode(node)
+        
+        dots.append(node)
+        
+        if dots.count >= 2 {
+            calculate()
+        }
+    }
+    
+    func calculate (){
+        let start = dots[0]
+        let end = dots[1]
+        
+        
+        let a = end.position.x - start.position.x
+        
+        let b = end.position.y - start.position.y
+        
+        let c = end.position.z - start.position.z
+        
+        let distance = sqrt(pow(a, 2) + pow(b, 2) + pow(c, 2))
+        
+        print(distance)
     }
     
     override func viewWillAppear(_ animated: Bool) {
